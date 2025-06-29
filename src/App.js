6b3bce0d8ab71ed8eab29cd6598ecef51ebca27d
carlosKauil <alexander_kauil@outlook.com>
2025-06-28 22:34:55 -0600

    prueba unity-webgl 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
+
 
 import { Unity, useUnityContext } from "react-unity-webgl";
-
-const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-  loaderUrl: "assets/game.loader.js",
-  dataUrl: "assets/game.data",
-  frameworkUrl: "assets/game.framework.js",
-  codeUrl: "assets/game.wasm",
-});
+import React from "react";
 
 function App() {
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "assets/game.loader.js",
+    dataUrl: "assets/game.data",
+    frameworkUrl: "assets/game.framework.js",
+    codeUrl: "assets/game.wasm",
+  });
+
   return (
-      <>
-    {!isLoaded && <p>Cargando {Math.round(loadingProgression * 100)}%</p>}
-    <Unity unityProvider={unityProvider} style={{ width: "100%", height: "100%" }} />
-  </>
+    <div>
+      {!isLoaded && <p>Cargando... {Math.round(loadingProgression * 100)}%</p>}
+      <Unity unityProvider={unityProvider} style={{ width: "100%", height: "100%" }} />
+    </div>
   );
 }
 
