25e1da260e997848a01ac0cdd3ce37c7506a29be
carlosKauil <alexander_kauil@outlook.com>
2025-06-28 22:12:04 -0600

    Inicializando Prueba de Unity

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <strong>Bienvenido Gente</strong>
-
+        <strong>PRUEBA UNITY</strong>
         <a
           className="App-link"
           href="https://reactjs.org"
